ff0741643a12be6f05e5e8e9417764ca9fa51748
Dzianis Matsiushyn <dzmats@gmail.com>
2020-11-06 13:36:58 +0100

    Неработающая версия Camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -2,24 +2,16 @@
  * @param {string} str
  * @returns {string}
  */
-function camelize(str) {
-  if (!str) {
-    return str;
+function camelize(str){
+  const splittedStr = str.split('');
+
+  for (let item of splittedStr){
+    if(item == '-'){
+      splittedStr[splittedStr.indexOf(item) + 1].toUpperCase();
+    }
   }
 
-  return str
-    .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
-    .map((part, index) => {
-      // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
-      // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
-      if (index === 0) { // Это нужно делать, чтобы не делать заглавной первую часть
-        return part;
-      }
 
-      let firstLetter = part.slice(0, 1);
-      let rest = part.slice(1);
-
-      return `${firstLetter.toUpperCase()}${rest}`;
-    })
-    .join('');
 }
+
+console.log(camelize('background-color'));
