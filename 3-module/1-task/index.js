58ff654896888107fe779b522ce027a0849996cc
Dzianis Matsiushyn <dzmats@gmail.com>
2020-11-08 20:23:00 +0100

    Namify completed

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -3,15 +3,5 @@
  * @returns {string[]}  объект
  */
 function namify(users) {
-  for (let user of users){
-    for (let key in user){
-
-      if ('name' in user){
-        names.push(user[key]);
-        continue;
-      }
-
-    }
-  }
-  return names;
+  return users.map(user => user.name)
 }
