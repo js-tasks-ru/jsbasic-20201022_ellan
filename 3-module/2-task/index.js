a0a4b89a9fc7e796833a1a39070de16172e55561
Dzianis Matsiushyn <dzmats@gmail.com>
2020-11-15 13:31:20 +0100

    filterRange

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,11 +4,8 @@
  * @param {number} b
  * @returns {number[]}
  */
-function filterRange(arr, a, b){
-	
-	arr.filter(
-		function(item){			
-			return (item >= a && item <= b);
-		}
-	);
+function filterRange(arr, a, b) {
+	return arr.filter((num) => {
+	  return a <= num && num <= b;
+	});
 }
