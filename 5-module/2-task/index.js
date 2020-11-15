0e5ab20fc850f765e7743de3a89fef7955ca193c
Dzianis Matsiushyn <dzmats@gmail.com>
2020-11-15 16:41:51 +0100

    toggleText

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,17 +1,11 @@
 function toggleText() {
-  // ваш код...
-  const button = document.querySelector('.toggle-text-button');
-  const div = document.getElementById('text');
 
-  button.addEventListener('click', handler1);
-  button.addEventListener('click', handler2);
-  
-  function handler1(){
-    div.hidden = true;
-  }
+  let toggleButton = document.querySelector('.toggle-text-button');
+  let textElement = document.getElementById('text');
 
-  function handler2(){
-    div.hidden = false;
-  }
-
+  toggleButton.addEventListener('click', () => {
+    let isTextElementHidden = textElement.hidden;
+    textElement.hidden = !isTextElementHidden;
+  });
 }
+
