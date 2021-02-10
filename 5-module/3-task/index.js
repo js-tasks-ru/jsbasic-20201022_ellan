78f2ee5db972f19774777fb71684edcb48729c1b
Dzianis Matsiushyn <ellan@tut.by>
2021-02-10 15:34:43 +0100

    Карусель пролог(update)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,35 +1,51 @@
 function initCarousel() {
-  const carouselInner = document.querySelector('.carousel__inner');
-  const rightArrow = document.querySelector('.carousel__arrow.carousel__arrow_right');
-  const leftArrow = document.querySelector('.carousel__arrow.carousel__arrow_left');
+  const container = document.querySelector('[data-carousel-holder]');
+  const carouselInner = container.querySelector('.carousel__inner');
+  const rightArrow = container.querySelector('.carousel__arrow_right');
+  const leftArrow = container.querySelector('.carousel__arrow_left');
   leftArrow.style.display = 'none';
   const shift = carouselInner.offsetWidth;
   let position = 0;
   let currentSlideNumber = 1;
   const slidesAmount = 4;
 
-  rightArrow.onclick = () => {
-    if (currentSlideNumber < 4) {
-      position -= shift;
-      carouselInner.style.transform = `translateX(${position}px)`;
-      currentSlideNumber++; 
-      leftArrow.style.display = '';
-    } 
-    if (currentSlideNumber == slidesAmount) {
-      rightArrow.style.display = 'none';
-    } 
-  };
-
-  leftArrow.onclick = () => {
-    if (currentSlideNumber > 1) {
-      position += shift;
-      carouselInner.style.transform = `translate(${position}px)`;
-      currentSlideNumber--;
-      rightArrow.style.display = '';
-    } 
-    if (currentSlideNumber == 1) {
-      leftArrow.style.display = 'none';
+  container.onclick = ({target}) => {
+    if (target.closest('.carousel__arrow_right')) {
+      if (currentSlideNumber < 4) {
+        right();
+        update(); 
+        leftArrow.style.display = '';
+      } 
+      if (currentSlideNumber == slidesAmount) {
+        rightArrow.style.display = 'none';
+      } 
     }
   };
+
+  container.onclick = ({target}) => {
+    if(target.closest('.carousel__arrow_left')) {
+      if (currentSlideNumber > 1) {
+        left();
+        update();
+        rightArrow.style.display = '';
+      } 
+      if (currentSlideNumber == 1) {
+        leftArrow.style.display = 'none';
+      }
+    } 
+  };
   
+  function left() {
+    position += shift;
+    currentSlideNumber--;
+  }
+
+  function right() {
+    position -= shift;
+    currentSlideNumber++; 
+  }
+
+  function update() {
+    carouselInner.style.transform = `translateX(${position}px)`;
+  }
 }
